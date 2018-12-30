188b2e2e5d5117e979b62bb67a09cb993dfb3927
ryuga4 <ryuga4@o2.pl>
2018-12-30 17:25:42 +0100

    minor tweak

--- CatRecognizer/app/src/main/java/pl/wmi/ino/catrecognizer/RandomResult.kt
+++ CatRecognizer/app/src/main/java/pl/wmi/ino/catrecognizer/RandomResult.kt
@@ -11,8 +11,6 @@ object RandomResult {
         val randomNumber : Double = Random().nextDouble()
         val pivot = 0.5 + (accumulator / 8.0)
         val result = randomNumber > pivot
-        Log.d("accumulator", accumulator.toString())
-        Log.d("pivot", pivot.toString())
         if (accumulator >= 0 && result) accumulator++
         else if (accumulator <= 0 && !result) accumulator--
         else accumulator=0
